b03bde1bfd971540bfcbb495b0970e66fe2be26c
Gravaldo <rizxyu5@gmail.com>
2024-06-23 21:29:48 +0700

    Add files via upload

---
+++ Pertemuan 4/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('toggle-dark-mode');
+
+    toggleButton.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+    });
+});
